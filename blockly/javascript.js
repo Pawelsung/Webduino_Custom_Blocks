d8dd3696d287bed3dcbb9472d4da9b813709d320
Paul Sung <151891447+Pawelsung@users.noreply.github.com>
2025-02-07 22:47:09 +0800

    Update javascript.js

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -1,9 +1,4 @@
 Blockly.JavaScript['get_time_ms'] = function(block) {
-  var code = 'new Date().getTime();';
-  return [code, Blockly.JavaScript.ORDER_ATOMIC];
-};
-
-Blockly.JavaScript['get_time_seconds'] = function(block) {
-  var code = 'Math.floor(Date.now() / 1000);';
+  var code = 'new Date().getTime();';  // 取得當前的毫秒時間戳
   return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
