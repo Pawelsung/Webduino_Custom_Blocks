39061f869470d5174106853192e02eaa1c4b7a81
Paul Sung <151891447+Pawelsung@users.noreply.github.com>
2025-02-07 23:39:27 +0800

    Update blocks.js

--- blockly/blocks.js
+++ blockly/blocks.js
@@ -1,10 +1,10 @@
-Blockly.Blocks['get_time_ms'] = {
+Blockly.Blocks['controls_get_time_ms'] = {
   init: function() {
     this.appendDummyInput()
         .appendField(Blockly.Msg["GET_TIME_MS"]);
     this.setOutput(true, "Number");
     this.setColour(230);
     this.setTooltip(Blockly.Msg["GET_TIME_MS"]);
-    this.setHelpUrl("");
+    this.setHelpUrl("https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getTime");
   }
 };
