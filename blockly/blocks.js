68d608bf1d453f3564df632d507bcd5ceac37561
Paul Sung <151891447+Pawelsung@users.noreply.github.com>
2025-02-07 22:33:27 +0800

    Update blocks.js

--- blockly/blocks.js
+++ blockly/blocks.js
@@ -8,14 +8,3 @@ Blockly.Blocks['get_time_ms'] = {
     this.setHelpUrl("");
   }
 };
-
-Blockly.Blocks['get_time_seconds'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField(Blockly.Msg["GET_TIME_SECONDS"]);
-    this.setOutput(true, "Number");
-    this.setColour(230);
-    this.setTooltip(Blockly.Msg["GET_TIME_SECONDS"]);
-    this.setHelpUrl("");
-  }
-};
