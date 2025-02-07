900e98018c5de301addd2672b514c7a1b29363ac
Paul Sung <151891447+Pawelsung@users.noreply.github.com>
2025-02-07 21:57:52 +0800

    Update blocks.js

--- blockly/blocks.js
+++ blockly/blocks.js
@@ -1,10 +1,21 @@
 Blockly.Blocks['get_time_ms'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField(Blockly.Msg["GET_TIME_MS"]);  // 使用語言變數
+        .appendField(Blockly.Msg["GET_TIME_MS"]);
     this.setOutput(true, "Number");
     this.setColour(230);
     this.setTooltip(Blockly.Msg["GET_TIME_MS"]);
+    this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['get_time_seconds'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(Blockly.Msg["GET_TIME_SECONDS"]);
+    this.setOutput(true, "Number");
+    this.setColour(230);
+    this.setTooltip(Blockly.Msg["GET_TIME_SECONDS"]);
     this.setHelpUrl("");
   }
 };
