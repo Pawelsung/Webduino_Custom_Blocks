60daa84af9665040fcf76a4cc86926252a213cff
Paul Sung <151891447+Pawelsung@users.noreply.github.com>
2025-02-07 21:57:35 +0800

    Update javascript.js

--- blockly/javascript.js
+++ blockly/javascript.js
@@ -2,3 +2,8 @@ Blockly.JavaScript['get_time_ms'] = function(block) {
   var code = 'new Date().getTime();';
   return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
+
+Blockly.JavaScript['get_time_seconds'] = function(block) {
+  var code = 'Math.floor(Date.now() / 1000);';
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+};
