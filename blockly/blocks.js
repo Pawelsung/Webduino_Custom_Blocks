44c9fe31283abf7930d22695b062104ca00fac5d
Paul Sung <151891447+Pawelsung@users.noreply.github.com>
2025-02-07 21:00:04 +0800

    Update blocks.js

--- blockly/blocks.js
+++ blockly/blocks.js
@@ -1,7 +1,7 @@
 Blockly.Blocks['get_time_ms'] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("取得目前時間（毫秒）");
+        .appendField(Blockly.Msg["GET_TIME_MS"]);  // 使用多語言翻譯
     this.setOutput(true, "Number");
     this.setColour(230);
     this.setTooltip("取得當前的時間戳記（毫秒）");
