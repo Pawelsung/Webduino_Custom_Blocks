6f12bbbf322729db123e992f69e64cee0fdf4bdc
Paul Sung <151891447+Pawelsung@users.noreply.github.com>
2025-02-07 22:46:44 +0800

    Update zh-hant.js

--- blockly/msg/blocks/zh-hant.js
+++ blockly/msg/blocks/zh-hant.js
@@ -1,8 +1,2 @@
-if (typeof Blockly === "undefined") var Blockly = { Msg: {} };
-
-Blockly.Msg.GET_TIME_MS = "取得目前時間（毫秒）";
-Blockly.Msg.GET_TIME_SECONDS = "取得目前時間（秒）";
-Blockly.Msg.GET_TIME_TIMESTAMP = "取得目前時間戳記";
-Blockly.Msg.GET_TIME_DATE = "取得目前日期";
-Blockly.Msg.GET_TIME_STRING = "取得目前時間字串";
-Blockly.Msg.GET_TIME_ZONE = "取得時區資訊";
+Blockly.Msg["catUtilities"] = "工具";
+Blockly.Msg["GET_TIME_MS"] = "取得目前時間（毫秒）";
