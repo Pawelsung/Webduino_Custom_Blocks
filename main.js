87bb2b36fb5030f27ad46532792e336ebc3de869
Paul Sung <151891447+Pawelsung@users.noreply.github.com>
2025-02-07 21:13:29 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+// 初始化 Webduino 需要的變數或函數
+console.log("main.js loaded successfully!");
+
+var myCustomVariable = "Hello Webduino!";
+function myCustomFunction() {
+    console.log("Custom function called!");
+}
